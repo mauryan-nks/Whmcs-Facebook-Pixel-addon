77e306719fa4cb8f84cb56ab685c873a3a2bbef3
SINHCOMS LLP <55934694+mauryan-nks@users.noreply.github.com>
2026-08-25 16:23:44 +0530

    Modernize addon config and admin page for WHMCS 9.0

--- facebook_pixel.php
+++ facebook_pixel.php
@@ -1,29 +1,52 @@
 <?php
 
-if (!defined("WHMCS")) {
-    exit("This file cannot be accessed directly");
+if (!defined('WHMCS')) {
+    exit('This file cannot be accessed directly');
 }
-function facebook_pixel_config()
-{
-    $config = [
-    	'name' => 'Facebook Pixel',
-    	'description' => ' Add Facebook Pixel Code',
-    	'version' => 'sncoms.77',
-    	'author' => 'Sinhcoms LLP',
-    	'language' => 'english',
-    	'fields' => [
-    		'code' => [
-    			'FriendlyName' => 'Facebook Pixel Code',
-    			'Type' => 'text',
-    			'Size' => '25',
-    			'Description' => 'get Facebook Pixel',
-    		]
-    	]
-    ];
 
-    return $config;
-}
-function facebook_pixel_output($vars)
+/**
+ * WHMCS addon module metadata and configuration fields.
+ */
+function facebook_pixel_config(): array
 {
-    echo "<br /><br />\n<p align=\"center\"><input type=\"button\" value=\"Launch Facebook Business Manager\" onclick=\"window.open('https://business.facebook.com/events_manager/?selected_data_sources=PIXEL','ganalytics');\" class=\"btn btn-primary btn-lg\" /></p>\n<br /><br />\n<p>Configuration of the Facebok Pixel Addon is done via <a href=\"configaddonmods.php\"><b>Setup > Addon Modules</b></a>. Please also ensure your active client area footer.tpl template file includes the {\$footeroutput} template tag.</p> <input type=\"button\" value=\"Watch how to Setup\" onclick=\"window.open('https://sncoms.co.in/setup');\" class=\"btn btn-primary btn-lg\" />";
+    return [
+        'name' => 'Meta (Facebook) Pixel',
+        'description' => 'Adds the Meta Pixel base code and selected ecommerce events to the WHMCS client area.',
+        'version' => '2.0.0',
+        'author' => 'Sinhcoms LLP / Nayan',
+        'language' => 'english',
+        'fields' => [
+            'code' => [
+                'FriendlyName' => 'Meta Pixel ID',
+                'Type' => 'text',
+                'Size' => '30',
+                'Description' => 'Enter only the numeric Meta Pixel ID (for example: 123456789012345).',
+            ],
+        ],
+    ];
+}
+
+/**
+ * Addon administration page.
+ */
+function facebook_pixel_output($vars): void
+{
+    echo <<<'HTML'
+<div class="container-fluid">
+    <div class="row">
+        <div class="col-md-10 col-lg-8">
+            <h2>Meta (Facebook) Pixel</h2>
+            <p>This addon injects the Meta Pixel base code into the WHMCS client area and tracks PageView, AddToCart, and InitiateCheckout events.</p>
+            <p>Configure the numeric Pixel ID under <strong>Configuration &gt; System Settings &gt; Addon Modules</strong>.</p>
+            <p>Your active client-area template must output WHMCS hook content in the standard head/footer locations.</p>
+            <p>
+                <a class="btn btn-primary" href="https://business.facebook.com/events_manager/" target="_blank" rel="noopener noreferrer">Open Meta Events Manager</a>
+            </p>
+            <div class="alert alert-info" role="alert">
+                Compatible with WHMCS 9.0 and PHP 8.2/8.3. For security, non-numeric Pixel IDs are ignored rather than rendered into the page.
+            </div>
+        </div>
+    </div>
+</div>
+HTML;
 }
